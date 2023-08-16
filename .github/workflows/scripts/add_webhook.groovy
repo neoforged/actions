ab45312984ccb7f685d1b6e9c8c685578d0d0063
Matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>
2023-08-16 14:49:38 +0300

    Update hook action

--- .github/workflows/scripts/add_webhook.groovy
+++ .github/workflows/scripts/add_webhook.groovy
@@ -3,16 +3,19 @@ import org.kohsuke.github.GHEvent
 import org.kohsuke.github.GHRepository
 import org.kohsuke.github.GitHub
 import org.kohsuke.github.GitHubBuilder
+import groovy.json.JsonSlurper
 
 @GrabResolver(name = 'central', root='https://repo1.maven.org/maven2/')
 @Grapes([
+    @Grab('org.apache.groovy:groovy-json:4.0.13'),
     @Grab('org.kohsuke:github-api:1.313')
 ])
 
+final hookStr = new JsonSlurper().parse(new java.io.File(System.getenv('EVENT_PATH'))).inputs.hook
 final GitHub gh = new GitHubBuilder()
         .withJwtToken(System.getenv('AUTH_TOKEN'))
         .build()
-final URL hook = new URL(System.getenv('HOOK_URL'))
+final URL hook = new URL(hookStr)
 final List<GHEvent> events = List.of(
         GHEvent.PUSH,
         GHEvent.PULL_REQUEST,
